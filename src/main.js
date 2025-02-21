6b6ba54f9d0085d5516f7ffdffd5b49b423cdc9b
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-21 19:39:58 +0530

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import vertex from "../shaders/vertex.glsl";
 import fragment from "../shaders/fragment.glsl";
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
@@ -20,17 +19,59 @@ const blobs = [
     {
         name: 'Color Fusion',
         background: '#9D73F7',
-        config: { "uPositionFrequency": 1, "uPositionStrength": 0.3, "uSmallWavePositionFrequency": 0.5, "uSmallWavePositionStrength": 0.7, "roughness": 1, "metalness": 0, "envMapIntensity": 0.5, "clearcoat": 0, "clearcoatRoughness": 0, "transmission": 0, "flatShading": false, "wireframe": false, "map": "cosmic-fusion" },
+        config: { 
+            "uPositionFrequency": 1, 
+            "uPositionStrength": 0.3, 
+            "uSmallWavePositionFrequency": 0.5, 
+            "uSmallWavePositionStrength": 0.7, 
+            "roughness": 1, 
+            "metalness": 0, 
+            "envMapIntensity": 0.5, 
+            "clearcoat": 0, 
+            "clearcoatRoughness": 0, 
+            "transmission": 0, 
+            "flatShading": false, 
+            "wireframe": false, 
+            "map": "cosmic-fusion" 
+        },
     },
     {
         name: 'Purple Mirror',
         background: '#5300B1',
-        config: { "uPositionFrequency": 0.584, "uPositionStrength": 0.276, "uSmallWavePositionFrequency": 0.899, "uSmallWavePositionStrength": 1.266, "roughness": 0, "metalness": 1, "envMapIntensity": 2, "clearcoat": 0, "clearcoatRoughness": 0, "transmission": 0, "flatShading": false, "wireframe": false, "map": "purple-rain" },
+        config: { 
+            "uPositionFrequency": 0.584, 
+            "uPositionStrength": 0.276, 
+            "uSmallWavePositionFrequency": 0.899, 
+            "uSmallWavePositionStrength": 1.266, 
+            "roughness": 0, 
+            "metalness": 1, 
+            "envMapIntensity": 2, 
+            "clearcoat": 0, 
+            "clearcoatRoughness": 0, 
+            "transmission": 0, 
+            "flatShading": false, 
+            "wireframe": false, 
+            "map": "purple-rain" 
+        },
     },
     {
         name: 'Alien Goo',
         background: '#45ACD8',
-        config: { "uPositionFrequency": 1.022, "uPositionStrength": 0.99, "uSmallWavePositionFrequency": 0.378, "uSmallWavePositionStrength": 0.341, "roughness": 0.292, "metalness": 0.73, "envMapIntensity": 0.86, "clearcoat": 1, "clearcoatRoughness": 0, "transmission": 0, "flatShading": false, "wireframe": false, "map": "lucky-day" },
+        config: { 
+            "uPositionFrequency": 1.022, 
+            "uPositionStrength": 0.99, 
+            "uSmallWavePositionFrequency": 0.378, 
+            "uSmallWavePositionStrength": 0.341, 
+            "roughness": 0.292, 
+            "metalness": 0.73, 
+            "envMapIntensity": 0.86, 
+            "clearcoat": 1, 
+            "clearcoatRoughness": 0, 
+            "transmission": 0, 
+            "flatShading": false, 
+            "wireframe": false, 
+            "map": "lucky-day" 
+        },
     },
     {
         name: 'Liquidity',
@@ -71,24 +112,24 @@ const blobs = [
         },
     },
     {
-    name: 'Fire Fly',
-    background: '#380B16',
-    config: { 
-        "uPositionFrequency": 1, 
-        "uPositionStrength": 1.5, 
-        "uSmallWavePositionFrequency": 1, 
-        "uSmallWavePositionStrength": 0.5, 
-        "roughness": 1, 
-        "metalness": 1, 
-        "envMapIntensity": 0, 
-        "clearcoat": 1, 
-        "clearcoatRoughness": 0, 
-        "transmission": 0, 
-        "flatShading": true, 
-        "wireframe": false, 
-        "map": "passion" 
+        name: 'Fire Fly',
+        background: '#380B16',
+        config: { 
+            "uPositionFrequency": 1, 
+            "uPositionStrength": 1.5, 
+            "uSmallWavePositionFrequency": 1, 
+            "uSmallWavePositionStrength": 0.5, 
+            "roughness": 1, 
+            "metalness": 1, 
+            "envMapIntensity": 0, 
+            "clearcoat": 1, 
+            "clearcoatRoughness": 0, 
+            "transmission": 0, 
+            "flatShading": true, 
+            "wireframe": false, 
+            "map": "passion" 
+        },
     },
-},
     {
         name: 'Back Bloosom',
         background: '#667174',
@@ -108,13 +149,10 @@ const blobs = [
             "map": "blackbloom"
         },
     },
-
-]
+];
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("#222");
-// const cameraDistance = 5;
-// const fov = 2 * Math.atan((window.innerHeight / 2) / cameraDistance) * (180 / Math.PI);
 const camera = new THREE.PerspectiveCamera(
     75, 
     window.innerWidth / window.innerHeight, 
@@ -126,7 +164,6 @@ camera.position.z = 3;
 const renderer = new THREE.WebGLRenderer({ 
     canvas: document.querySelector('.canvas'),
     antialias: true,
-    // alpha:true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -134,21 +171,10 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1;
 renderer.outputEncoding = THREE.sRGBEncoding;
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-
-// Load HDRI environment map
-
-// rgbeLoader.load('/hdri/peppermint_powerplant_1k.hdr', (texture) => {
-//     texture.mapping = THREE.EquirectangularReflectionMapping;
-//     scene.environment = texture;
-// });
-
 rgbeLoader.load('/hdri/studio1k.hdr', (texture) => {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     scene.environment = texture;
 });
-
-
 
 const uniforms = {
     uTime: { value: 1 },
@@ -158,8 +184,8 @@ const uniforms = {
     uSmallWavePositionFrequency: { value: blobs[currentIndex].config.uSmallWavePositionFrequency },
     uSmallWavePositionStrength: { value: blobs[currentIndex].config.uSmallWavePositionStrength },
     uSmallWaveTimeFrequency: { value: 0.3 },
-    roughness: { value: blobs[currentIndex].config.roughness }, // Updated roughness uniform
-    metalness: { value: blobs[currentIndex].config.metalness },   // Updated metalness uniform
+    roughness: { value: blobs[currentIndex].config.roughness },
+    metalness: { value: blobs[currentIndex].config.metalness },
     envMapIntensity: {value : blobs[currentIndex].config.envMapIntensity},
     clearcoat: {value : blobs[currentIndex].config.clearcoat},
     clearcoatRoughness: {value : blobs[currentIndex].config.clearcoatRoughness},
@@ -168,22 +194,12 @@ const uniforms = {
     wireframe: {value : blobs[currentIndex].config.wireframe},
 };
 
-// const gui = new GUI();
-// gui.add(uniforms.uPositionFrequency, 'value', 0, 5).name('Position Frequency');
-// gui.add(uniforms.uPositionStrength, 'value', 0, 5).name('Position Strength');
-// gui.add(uniforms.uTimeFrequency, 'value', 0, 5).name('Time Frequency');
-// gui.add(uniforms.uSmallWavePositionFrequency, 'value', 0, 5).name('SWPositionFrequency');
-// gui.add(uniforms.uSmallWavePositionStrength, 'value', 0, 5).name('SWPositionStrength');
-// gui.add(uniforms.uSmallWaveTimeFrequency, 'value', 0, 5).name('SWTimeFrequency');
-// gui.add(uniforms.roughness, 'value', 0, 1).name('Roughness'); // GUI for roughness
-// gui.add(uniforms.metalness, 'value', 0, 1).name('Metalness'); // GUI for metalness
-
 const texture = textureLoader.load(`./gradient/${blobs[currentIndex].config.map}.png`);
 texture.minFilter = THREE.LinearMipMapLinearFilter; 
 texture.magFilter = THREE.LinearFilter;
 texture.colorSpace = THREE.SRGBColorSpace;
 
-const geometry = new THREE.IcosahedronGeometry(1 , 108);
+const geometry = new THREE.IcosahedronGeometry(1.07 , 108);
 const material = new CustomShaderMaterial({
     baseMaterial: THREE.MeshPhysicalMaterial,
     vertexShader: vertex,
@@ -195,8 +211,10 @@ const material = new CustomShaderMaterial({
     clearcoat: blobs[currentIndex].config.clearcoat,
     clearcoatRoughness: blobs[currentIndex].config.clearcoatRoughness,
     transmission: blobs[currentIndex].config.transmission,
-    flatShading: blobs[currentIndex].config.flatShading,
-    wireframe: blobs[currentIndex].config.wireframe,
+    // flatShading: blobs[currentIndex].config.flatShading,
+    // wireframe: blobs[currentIndex].config.wireframe,
+    flatShading: blobs[currentIndex].config.flatShading, // Correct here
+    wireframe: blobs[currentIndex].config.wireframe // Correct here
 });
 
 const mergedGeometry = mergeVertices(geometry);
@@ -235,13 +253,12 @@ const texts = blobs.map((blob, index)=> {
     if (index !== 0) myText.scale.set(0,0,0); 
     myText.letterSpacing = -0.07;
     myText.fontSize = window.innerWidth / 4000;
-    myText.glyphGeometryDetail = 50;
+    myText.fontWeight = 'bold';
+    myText.glyphGeometryDetail = 30;
     myText.sync();
     scene.add(myText);
     return myText;
 })
-
-
 
 window.addEventListener("wheel", (e) => {
     if(isAnimating) return;
@@ -276,7 +293,6 @@ window.addEventListener("wheel", (e) => {
         ease : "power2.inOut"
     })
 
-
     gsap.to(blob.rotation, {
         y : blob.rotation.y + Math.PI * 4 * -direction,
         duration : 1.5,
@@ -293,10 +309,6 @@ window.addEventListener("wheel", (e) => {
     })
 
     updateBlob(blobs[next].config);
-
-    // setTimeout(() => {
-    //     isAnimating = false;
-    // }, 2000);
 })
 
 function updateBlob(config) {
@@ -307,7 +319,6 @@ function updateBlob(config) {
     if (config.uSmallWaveTimeFrequency !== undefined) gsap.to(material.uniforms.uSmallWaveTimeFrequency, { value: config.uSmallWaveTimeFrequency, duration: 1, ease: 'power2.inOut' });
     if (config.map !== undefined) {
       setTimeout(() => {
-        // material.map = textureLoader.load(`./gradient/${config.map}.png`);
         const newTexture = textureLoader.load(`./gradient/${config.map}.png`);
         newTexture.minFilter = THREE.LinearMipMapLinearFilter;
         newTexture.magFilter = THREE.LinearFilter;
@@ -323,15 +334,12 @@ function updateBlob(config) {
     if (config.transmission !== undefined) gsap.to(material, { transmission: config.transmission, duration: 1, ease: 'power2.inOut' });
     if (config.flatShading !== undefined) gsap.to(material, { flatShading: config.flatShading, duration: 1, ease: 'power2.inOut' });
     if (config.wireframe !== undefined) gsap.to(material, { wireframe: config.wireframe, duration: 1, ease: 'power2.inOut' });
-  }
+}
 
 loadingManager.onLoad = function () {
     function animate() {
         requestAnimationFrame(animate);
         uniforms.uTime.value = clock.getElapsedTime();
-        // controls.update();
-        // material.roughness = uniforms.roughness.value; // Update material roughness
-        // material.metalness = uniforms.metalness.value; // Update material metalness
         renderer.render(scene, camera);
     }
     const bg = new THREE.Color(blobs[currentIndex].background);
@@ -344,14 +352,10 @@ loadingManager.onLoad = function () {
     })
 
     animate();
-    console.log('Loading complete!');
 };
 loadingManager.onStart = function (url, itemsLoaded, itemsTotal) {
-    console.log(`Started loading: ${url}. Loaded ${itemsLoaded} of ${itemsTotal} files.`);
 };
 loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
-    console.log(`Loading: ${url}. Loaded ${itemsLoaded} of ${itemsTotal} files.`);
 };
 loadingManager.onError = function (url) {
-    console.error(`There was an error loading ${url}`);
 };
